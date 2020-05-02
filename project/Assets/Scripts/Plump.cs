6f4bf3c8681fd39830f64ec9337819dee346efbc
shrnkm <sahar.niknam@gmail.com>
2020-05-02 14:08:40 +0200

    add unity basic files

---
+++ project/Assets/Scripts/Plump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plump : MonoBehaviour
+{
+
+    private Rigidbody pl;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pl = GetComponent<Rigidbody>();
+        pl.GetComponent<Renderer>().material.color = new Color(250, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        pl.AddForce (movement * 10);
+
+    }
+}
---
+++ project/Assets/Scripts/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject plump;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - plump.transform.position;
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = plump.transform.position + offset;
+    }
+}
