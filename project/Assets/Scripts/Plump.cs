f4b47f787cef114bd1518fc9f55458783cdb5626
shrnkm <sahar.niknam@gmail.com>
2020-05-27 10:25:24 +0200

    update unity project

--- project/Assets/Scripts/Plump.cs
+++ project/Assets/Scripts/Plump.cs
@@ -1,28 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Plump : MonoBehaviour
 {
 
+    // player
     private Rigidbody pl;
+    // movement sound effect
+    public AudioSource roll;
+    private AudioClip rolling;
+    bool isRolling = false;
     
-    // Start is called before the first frame update
+    
     void Start()
     {
+        // get the player object
         pl = GetComponent<Rigidbody>();
         pl.GetComponent<Renderer>().material.color = new Color(250, 0, 0, 0);
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    
+    void Update()
     {
+        // player movement
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
-
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        pl.position = pl.position + 0.2f * movement;
+        
+        
+        // player movement sound effect
+        if ((moveHorizontal != 0) || (moveVertical != 0))
+            isRolling = true;
+        else
+            isRolling = false;
+            
+        if (isRolling)
+        {
+            roll.clip = rolling;
+            roll.Play();
+        }
 
-        pl.AddForce (movement * 10);
+    }
 
+    void FixedUpdate()
+    {
+        
     }
 }
